b03524c7a1254fd12c96285f7b0e9a48c28ef77b
Mingzhe Li <mingzhe.li@spatialvision.com.au>
2022-07-06 11:28:17 +1000

    feat(amplify-pd-project): create the grapql api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AmplifyAuthenticatorModule } from "@aws-amplify/ui-angular";
 
+import { Amplify, API, graphqlOperation } from "aws-amplify";
+import { AmplifyAuthenticatorModule } from "@aws-amplify/ui-angular";
 // @ts-ignore
 import awsconfig from "../aws-exports"
-import { Amplify } from "aws-amplify";
 
 Amplify.configure(awsconfig);
 
